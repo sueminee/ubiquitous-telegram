8f91bad152550cf7ea3f889f8ac973f28f651a59
Suemin Jeong <sueminee@gmail.com>
2020-04-15 20:59:56 +0900

    add slider useing react-id-swiper

--- testing-carousel/src/App.js
+++ testing-carousel/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+
 import Slider from './components/Slider';
+import CustomBuildSwiper from './components/custom';
+// import 'react-id-swiper/lib/styles/scss/swiper.scss';
+import 'swiper/swiper.scss'
+import './style.scss';
+
 
 function App() {
   return (
     <div className="App">
       <Slider />
+      {/* <CustomBuildSwiper /> */}
     </div>
   );
 }
